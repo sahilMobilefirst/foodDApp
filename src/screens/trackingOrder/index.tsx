dea2363b697ba7ee650fdbde6a43802593b2cd4f
@sahilMobilefirst <sahil@mobilefirstapplications.com>
2024-03-13 14:12:56 +0530

    used react-native-persmissions and also added the linking to locations settings

--- src/screens/trackingOrder/index.tsx
+++ src/screens/trackingOrder/index.tsx
@@ -1,7 +1,6 @@
-import { Dimensions, Image, PermissionsAndroid, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Pressable, StyleSheet, View, Platform, Text, Linking } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { BottomDrawer } from '../../components/BottomDrawer';
-import { mapImg } from '../../../assets/images';
 import CartNav from '../../components/Navbars/cartNav';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../navigation';
@@ -9,58 +8,70 @@ import MapView, { Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import MapViewDirections from 'react-native-maps-directions';
 import { GOOGLE_API_KEY } from '../../utils/data';
+import { PERMISSIONS, request, check } from 'react-native-permissions';
 
 type TrackingProps = NativeStackScreenProps<RootStackParamList, 'Tracking'>;
 
 const OrderTracking = ({ navigation }: TrackingProps) => {
-  const [yourLatitude,setLatitude] = useState<number>(21.1557);
-  const [yourLongitude,setLongitude] = useState<number>(79.0849);
-  
-  useEffect(()=>{
-    const requestCameraPermission = async () => {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          {
-            title: 'LOCATION Permission',
-            message:
-              'App need permission for tracking order Location',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-          },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('You can use the camera');
-        } else {
-          console.log('Location permission denied');
-        }
-      } catch (err) {
-        console.log(err);
-        
-      }
-    };
-    requestCameraPermission()
-    getLocation()
-  },[])
-  
-  const yourLocationPoint = { latitude:yourLatitude , longitude:yourLongitude  };
+  const [yourLatitude, setLatitude] = useState<number>(21.1557);
+  const [yourLongitude, setLongitude] = useState<number>(79.0849);
+  const yourLocationPoint = { latitude: yourLatitude, longitude: yourLongitude };
   const startingPoint = { latitude: 21.1565, longitude: 79.0717 }; 
 
-
-  const getLocation = ()=>{
+  const getLocation = () => {
     Geolocation.getCurrentPosition(
       (position) => {
         console.log(position);
-        setLatitude(position.coords.latitude)
-        setLongitude(position.coords.longitude)
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
       },
       (error) => {
         console.log(error.code, error.message);
       },
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-  );
+    );
   }
+
+  
+  const requestLocationPermission = async () => {
+    try {
+      let permissionResult;
+      if (Platform.OS === 'ios') {
+        permissionResult = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      } else {
+        permissionResult = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      }
+      
+      if (permissionResult === 'granted') {
+        console.log('Location permission already granted');
+        getLocation();
+      } else {
+        console.log('Location permission denied');
+        openLocationSettings();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const openLocationSettings = async () => {
+    try {
+      if (Platform.OS === 'android') {
+        await Linking.openSettings();
+      } else if (Platform.OS === 'ios') {
+        await Linking.openURL('App-Prefs:LOCATION_SERVICES');
+      }
+    } catch (error) {
+      console.error('Error opening location settings:', error);
+    }
+  };
+  
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  
 
   return (
     <View style={styles.mainCon}>
@@ -90,12 +101,12 @@ const OrderTracking = ({ navigation }: TrackingProps) => {
           pinColor="green"
         />
         <MapViewDirections
-        origin={startingPoint}
-        destination={yourLocationPoint}
-        apikey={GOOGLE_API_KEY}
-        strokeWidth={2}
-        strokeColor='blue'
-      />
+          origin={startingPoint}
+          destination={yourLocationPoint}
+          apikey={GOOGLE_API_KEY}
+          strokeWidth={2}
+          strokeColor='blue'
+        />
       </MapView>
       <BottomDrawer/>
     </View>
@@ -119,12 +130,12 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height * 0.85,
     width: Dimensions.get('window').width * 1,
   },
-  getlocation:{
-    width:Dimensions.get('window').width * 0.9,
-    height:50,
-    alignSelf:"center",
-    position:"absolute",
-    bottom:50,
-    backgroundColor:"orange"
+  getlocation: {
+    width: Dimensions.get('window').width * 0.9,
+    height: 50,
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: 50,
+    backgroundColor: 'orange'
   }
 });
